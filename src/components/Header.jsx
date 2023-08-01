371e0aa40f4f5b61a87dbc674bc80ef4937a8fd3
leen1515 <leventredeparis@gmail.com>
2023-08-01 15:14:01 +0200

    feat(navigation): style + lien actif

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,27 @@ import { NavLink, Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 function Header () {
-  return <header>
+  return <header className='home-header'>
     <Link to="/">
-          <img src={logo} alt="Logo de Kasa" />
-        </Link>
-        <nav>
-          <ul>
-            <li>
-              <NavLink
-                to="/" >
-                Accueil
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-              >à propos
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-    </header>;
+      <img src={logo} alt="Logo de Kasa" />
+    </Link>
+    <nav className='navigation'>
+      <ul>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+            to="/" >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
+            to="/about"
+          >A Propos
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>;
 }
 
 export default Header;
