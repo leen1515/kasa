b54724701dab72f46baeb6e63de4c8c5c85ee813
leen1515 <leventredeparis@gmail.com>
2023-08-01 11:58:11 +0200

    feat(banner): ajout bannière

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import banner from '../assets/image-banner.png';
+
+function Banner () {
+  return <><h1>Banner</h1>
+  <img src = { banner }>
+  </img>;
+            </>;
+}
+
+export default Banner;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import Header from './Header';
 import Footer from './Footer';
+import Banner from './Banner';
 
 function Home () {
   return <>
   <Header/>
+  <main>
+  <Banner/>
+  </main>
   <Footer/>
             </>;
 }
