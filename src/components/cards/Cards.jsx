290ca6f54b53be295df3c9af09fe45418c110b43
leen1515 <leventredeparis@gmail.com>
2023-08-11 12:34:26 +0200

    feat(style): affichage vignette des logements + leur conteneur

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 
 function Cards (props) {
-  return <Link to={`/logements/${props.id}`}><figure>
-                <img src={props.cover} alt='logement-illustration' />
-                <figcaption> {props.title} </figcaption>
+  return <Link className= "card" to={`/logements/${props.id}`}><figure className="card__figure">
+                <img className="figure__cover" src={props.cover} alt='logement-illustration' />
+                <div className="figure__filter"></div>
+                <figcaption className="figure__title"> {props.title} </figcaption>
             </figure>
         </Link>;
 }
