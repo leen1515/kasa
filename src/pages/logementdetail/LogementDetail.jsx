437bb2f0033ffdeef190593089bc8a356e3202e5
leen1515 <leventredeparis@gmail.com>
2023-08-21 00:49:50 +0200

    chore(): update dependance

--- src/pages/logementdetail/LogementDetail.jsx
+++ src/pages/logementdetail/LogementDetail.jsx
@@ -1,31 +1,41 @@
 import Collapse from '../../components/collapse/Collapse';
 import Gallery from '../../components/gallery/Gallery';
 import Tags from '../../components/tag/Tags';
+import { useContext, useEffect, useState } from 'react';
+import GetDataContext from '../../utils/GetDataContext';
+import { useParams } from 'react-router';
+import GetStars from '../../components/starts/GetStars';
 
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
-
-function LogementDetail ({ logementList }) {
+function LogementDetail () {
+  const logementList = useContext(GetDataContext).data;
   const { logementID } = useParams();
-  const [logementSelected, setLogementSelected] = useState([]);
+  const [logementSelected, setLogementSelected] = useState(null);
+
   useEffect(() => {
-    const getLogement = async () => {
-      setLogementSelected(logementList.find((logement) => logement.id === logementID));
-    };
-    getLogement();
-  }, []);
-  return <>
-    <Header/>
-    <main>
-        <Gallery logementList = {logementList} logementID = {logementID}/>
-        <Tags liste = {logementList} id = { logementID }/>
-  <Collapse label = "description" detail = { logementSelected.description } />
-      <Collapse label = "équipement" detail = { logementSelected.equipments } />
-      <Footer/>
-      </main>
+    const selected = logementList.find((logement) => logement.id === logementID);
+
+    if (selected) {
+      setLogementSelected(selected);
+    } else {
+      console.log('Logement non trouvé.');
+    }
+  }, [logementID, logementList]);
+
+  return <>{ logementSelected &&
+    <main className = "main-logement">
+        <Gallery logementID = { logementID }/>
+        <Tags logementID = { logementID }/>
+        <figure>
+          <figcaption>{logementSelected.host.name}</figcaption>
+          <img src={logementSelected.host.picture} alt="Photo de l'hôte"/>
+        </figure>
+
+        <GetStars logementID = { logementID }/>
+        <div className="collapses-logement">
+        <Collapse label = "description" detail = { logementSelected.description } />
+        <Collapse label = "équipement" detail = { logementSelected.equipments } />
+        </div>
+      </main>}
     </>;
 }
-
 export default LogementDetail;
